081a8542706edcb2d1a2fa3cf81967f4826850e5
SigmaHyperon <nano_tech@web.de>
2026-01-01 01:37:32 +0100

    initial commit

---
+++ src/utils.ts
@@ -0,0 +1,33 @@
+import fs from "fs";
+import path from "path";
+
+export class Input {
+    content: string;
+    constructor(input: string) {
+        this.content = input;
+    }
+    static load(day: number): Input {
+        const filename = `${day.toString().padStart(2, "0")}.txt`;
+        const filepath = path.join("input", filename);
+        return new Input(fs.readFileSync(filepath).toString());
+    }
+
+    asLines(): Lines<string> {
+        return new Lines(this.content.replace(/^\s+|\s+$/g, '').split('\n'));
+    }
+}
+
+export class Lines<T> {
+    content: T[];
+    constructor(input: T[]) {
+        this.content = input;
+    }
+
+    parse<K>(parser: (arg0: T) => K): Lines<K> {
+        return new Lines(this.content.map(v => parser(v)));
+    }
+
+    asArray(): T[] {
+        return [...this.content];
+    }
+}
